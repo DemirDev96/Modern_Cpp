39e3ac81db9861020334faa420b8fae6ee8dcea1
DemirDev96 <demir.dev.1996@gmail.com>
2026-01-26 14:38:58 +0000

    Create structs.cpp

---
+++ structs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void DrawLine(int x1, int y1, int x2, int y2){
+
+}
+struct Point
+{
+    int x = 0;
+    int y = 0;
+};
+
+void DrawLine(Point start, Point end){
+    std::cout << start.x << std::endl;
+}
+
+int main(){
+
+    
+
+    return 0;
+}
